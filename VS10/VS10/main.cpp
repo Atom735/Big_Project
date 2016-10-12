c48fce5a7eba0aba22515f72ccd0db93e8b42e1a
Adel Gilyazeev <Atom735.2013@gmail.com>
2016-10-12 14:50:33 +0300

    Update Test's with SDL

--- VS10/VS10/main.cpp
+++ VS10/VS10/main.cpp
@@ -4,65 +4,77 @@
 #include <SDL.h>
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
 #define _USE_MATH_DEFINES
 #include <cmath>
 
-SDL_Window    *g_Window           = 0;
-SDL_Surface   *g_ScreenSurface    = 0;
-SDL_Thread    *g_ThreadVideo      = 0;
+SDL_Window    *g_Window            = 0;
+SDL_Surface   *g_ScreenSurface     = 0;
+SDL_Thread    *g_ThreadVideo       = 0;
+SDL_Thread    *g_ThreadVideoSwaper = 0;
+int           *g_ScreenBufferBack  = 0;
+int           *g_ScreenBufferFront = 0;
+typedef void (* gVideoFunc) (void);
+std::vector<gVideoFunc>
+               g_VideoList;
 
-static double r_Time()
+float r_Time()
+{return ((float)(SDL_GetPerformanceCounter()))/((float)(SDL_GetPerformanceFrequency()));}
+
+void r_SreenBuffersSwap()
+{int *b=g_ScreenBufferBack;g_ScreenBufferBack=g_ScreenBufferFront;g_ScreenBufferFront=b;}
+
+void r_ScreenBufferClear()
+{memset(g_ScreenBufferBack, 2048*2048*sizeof(int), 0);}
+
+void r_ScreenLine()
 {
-	return ((double)(SDL_GetPerformanceCounter()))/((double)(SDL_GetPerformanceFrequency()));
+	for(int y=0;y<600;y++)
+		for(int x=0;x<800;x++)
+			g_ScreenBufferBack[y*2048+x]=0xff*(!(int(r_Time())&1));
 }
 
-float exponent = 1.0;
-float rings = 10.0;
-float speed = 10.1;
-float time = 0;
-
-
-static int r_Color(int ix, int iy, const float iw, const float ih)
+int r_ThreadVideoMain(void *ptr)
 {
-	float x = (((((float)(ix))+0.5f)/((float)(iw)))-0.5f)*2.0f;
-	float y = (((((float)(iy))+0.5f)/((float)(ih)))-0.5f)*2.0f;
-	
-	float ang = atan2(x, y);
-	float rad = pow(x*x+y*y, exponent);
-
-	return int((0.5f * (1.0f + sin(ang + rings * rad + speed * time)))*255.0f);
+	int *run = (int*)(ptr);	
+	while(*run)
+	{
+		for(int i=0; i<g_VideoList.size(); i++)
+			g_VideoList[i]();		
+		r_SreenBuffersSwap();
+	}
+	return 0;
 }
-
-
-static int r_ThreadVideoMain(void *ptr)
+int r_ThreadVideoSwaperMain(void *ptr)
 {
-	while(*(int*)(ptr))
+	int *run = (int*)(ptr);
+	g_ScreenBufferBack = (int*)malloc(2048*2048*sizeof(int));
+	g_ScreenBufferFront = (int*)malloc(2048*2048*sizeof(int));
+	r_ScreenBufferClear();
+	r_SreenBuffersSwap();
+	r_ScreenBufferClear();
+	g_VideoList.clear();
+	g_VideoList.push_back(r_ScreenBufferClear);
+	g_VideoList.push_back(r_ScreenLine);
+	g_ThreadVideo = SDL_CreateThread(r_ThreadVideoMain, "ThreadVideoDrawer", ptr);
+	while(*run)
 	{
 		g_ScreenSurface = SDL_GetWindowSurface( g_Window );
 		int w=g_ScreenSurface->w;
 		int h=g_ScreenSurface->h;
 		int *p=(int*)(g_ScreenSurface->pixels);
-		
-		float time1 = r_Time();
-		time = r_Time();
 		SDL_LockSurface(g_ScreenSurface);
-		for(int y=0;y<h && (time1+0.5f > time);y++)
+		for(int y=0;y<h;y++)
 		{
-			if(y%3==2)
-			{
-				SDL_UnlockSurface(g_ScreenSurface);
-				SDL_UpdateWindowSurface( g_Window );
-				SDL_LockSurface(g_ScreenSurface);
-				time = r_Time();
-			}
-			for(int x=0;x<w;x++)
-			{
-				p[y*w+x] = r_Color(x,y,w,h);
-			}
+			memcpy(p+y*w, g_ScreenBufferFront+2048*y, sizeof(int)*w);
 		}
 		SDL_UnlockSurface(g_ScreenSurface);
 		SDL_UpdateWindowSurface( g_Window );
 	}
+	SDL_WaitThread(g_ThreadVideo,run);
+	free(g_ScreenBufferFront);
+	free(g_ScreenBufferBack);
+	g_ScreenBufferBack=0;
     return 0;
 }
 
@@ -87,7 +99,7 @@ int wmain(int argc, char const *argv[])
 
 
 	int l_ReturnValue=1;
-	g_ThreadVideo = SDL_CreateThread(r_ThreadVideoMain, "ThreadVideos", (void*)(&l_ReturnValue));
+	g_ThreadVideoSwaper = SDL_CreateThread(r_ThreadVideoSwaperMain, "ThreadVideoSwaper", (void*)(&l_ReturnValue));
 
 	SDL_Event e;
 	while(l_ReturnValue)
@@ -106,7 +118,7 @@ int wmain(int argc, char const *argv[])
 		}
 	}
 
-	SDL_WaitThread(g_ThreadVideo,&l_ReturnValue);
+	SDL_WaitThread(g_ThreadVideoSwaper,&l_ReturnValue);
 	SDL_UpdateWindowSurface( g_Window );
     SDL_DestroyWindow( g_Window );
     SDL_Quit();
